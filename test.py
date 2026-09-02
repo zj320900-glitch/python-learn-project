8fee2e1a833d367c0dcbba4ec61d3d1b42bebda7
zj320900-glitch <zj320900@gmail.com>
2026-09-02 09:49:47 +0800

    2026-09-02

--- test.py
+++ test.py
@@ -1,2 +1,12 @@
 print ("test")
 print ("123")
+def is_valid_identifier(name):
+    try:
+        exec(f"{name} = None")
+        return True
+    except:
+        return False
+
+print(is_valid_identifier("2var"))  # False
+print(is_valid_identifier("var2"))  # True
+
