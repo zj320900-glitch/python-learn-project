2aeb177564c29ee310344fd33daceb7f69c62fc9
zj320900-glitch <zj320900@gmail.com>
2026-09-03 17:19:42 +0800

    python 学习

--- test.py
+++ test.py
@@ -10,3 +10,29 @@ def is_valid_identifier(name):
 print(is_valid_identifier("2var"))  # False
 print(is_valid_identifier("var2"))  # True
 
+str="123456789"
+print(str)
+print(str[0:-1])  #最后一个数字不生效
+print(str[0])
+print(str[2:5])
+print(str[2:])
+print(str[1:6:2])
+print(str*2)
+print(str+'你好')
+print('hello\nnihaoya')
+print(r'hello\nnihaoya')
+print('\n')
+print(r'\n')
+#input('\n\n按下enter键后退出\n')
+import sys;x='runoob';sys.stdout.write(x+'\n')
+x="a";y="b"
+print(x)
+print(y)
+print(x,end="")
+print(y,end="")
+import sys
+print("命令行参数为：")
+for i in sys.argv:
+    print(i)
+print ("\n python路径为：",sys.path)
+$ python -h 
