4eb7993ca8103037f72e4ba4061f34fd84160bdc
zj320900-glitch <zj320900@gmail.com>
2026-09-04 09:49:02 +0800

    python运算符

--- test.py
+++ test.py
@@ -1,38 +1,35 @@
-print ("test")
-print ("123")
-def is_valid_identifier(name):
-    try:
-        exec(f"{name} = None")
-        return True
-    except:
-        return False
+a = 21
+b = 10
+c = 0
+if ( a == b ):
+    print ( "1-a等于b" )
+else:
+    print ( "1-a不等于b" )
 
-print(is_valid_identifier("2var"))  # False
-print(is_valid_identifier("var2"))  # True
+if ( a != b ):
+    print ( "2-a不等于b" )
+else:
+    print ( "2-a等于b" )
+if ( a < b ):
+    print ( "3-a小于b" )
+else:
+    print ( "3-a大于等于b" )
+if ( a > b):
+    print ( "4-a大于b" )
+else:
+    print ( "4-a小于等于b" )
+a = 5
+b = 20
+if ( a <= b ):
+    print ( "5-a小于等于b" )
+else:
+    print ( "5-a大于b")
+if ( a >= b ):
+    print ( "6-a大于等于b" )
+else:
+    print ( "6-a小于b" )
 
-str="123456789"
-print(str)
-print(str[0:-1])  #最后一个数字不生效
-print(str[0])
-print(str[2:5])
-print(str[2:])
-print(str[1:6:2])
-print(str*2)
-print(str+'你好')
-print('hello\nnihaoya')
-print(r'hello\nnihaoya')
-print('\n')
-print(r'\n')
-#input('\n\n按下enter键后退出\n')
-import sys;x='runoob';sys.stdout.write(x+'\n')
-x="a";y="b"
-print(x)
-print(y)
-print(x,end="")
-print(y,end="")
-import sys
-print("命令行参数为：")
-for i in sys.argv:
-    print(i)
-print ("\n python路径为：",sys.path)
-$ python -h 
+
+
+
+
