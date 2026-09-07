6a9d55322dd112664f7373557f033eb25284d2ce
zj320900-glitch <zj320900@gmail.com>
2026-09-07 17:45:44 +0800

    阶段0练习题

--- test.py
+++ test.py
@@ -1,35 +1,13 @@
-a = 21
-b = 10
-c = 0
-if ( a == b ):
-    print ( "1-a等于b" )
-else:
-    print ( "1-a不等于b" )
 
-if ( a != b ):
-    print ( "2-a不等于b" )
-else:
-    print ( "2-a等于b" )
-if ( a < b ):
-    print ( "3-a小于b" )
-else:
-    print ( "3-a大于等于b" )
-if ( a > b):
-    print ( "4-a大于b" )
-else:
-    print ( "4-a小于等于b" )
-a = 5
-b = 20
-if ( a <= b ):
-    print ( "5-a小于等于b" )
-else:
-    print ( "5-a大于b")
-if ( a >= b ):
-    print ( "6-a大于等于b" )
-else:
-    print ( "6-a小于b" )
+char = "*"
+n =1
+while n <= 10:
+    print (char * n)
+    n = n + 1
 
-
+name = input("请输入您的姓名：")
+age = input("请输入您的年龄：")
+print ("您好：", name, "您今年", age, "岁")
 
 
 
