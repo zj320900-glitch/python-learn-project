4f1de1986e6ad49e244462ba0b0351691f7d06cc
zj320900-glitch <zj320900@gmail.com>
2026-09-09 19:18:22 +0800

    开始os模块

--- test.py
+++ test.py
@@ -1,13 +1,1 @@
-
-char = "*"
-n =1
-while n <= 10:
-    print (char * n)
-    n = n + 1
-
-name = input("请输入您的姓名：")
-age = input("请输入您的年龄：")
-print ("您好：", name, "您今年", age, "岁")
-
-
-
+print ("开始os模块")
